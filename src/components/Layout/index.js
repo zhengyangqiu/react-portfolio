764cce1c8b7ae41f7156790fb6ba648d2570f9f8
zhengyang qiu <15224003720@163.com>
2022-03-30 23:57:55 +0100

    add sidebar

---
+++ src/components/Layout/index.js
@@ -0,0 +1,16 @@
+import './index.scss';
+import SiderBar from '../SiderBar';
+
+const Layout = ()=>{
+    return ( 
+    <>
+    <SideBar/>
+    </>
+    )
+}
+
+
+export default Layout
+
+//add comment 
+
---
+++ src/components/SiderBar/index.js
@@ -0,0 +1,9 @@
+import './index.scss';
+
+const SiderBar = ()=>(
+    <>Hello from sidebar</>
+)
+
+
+export default SiderBar;
+
