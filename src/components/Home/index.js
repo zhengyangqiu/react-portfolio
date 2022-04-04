1c9b5b8190c7994cce8d8ed3e564f0e921df0197
zhengyang qiu <15224003720@163.com>
2022-04-04 23:56:42 +0100

    add logo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import './index.scss';
 import { useState, useEffect } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
 
 const Home=()=>{
 
@@ -40,6 +41,7 @@ const Home=()=>{
                 <Link to ="/contact" className="flat-button"> CONTACT ME</Link> 
                 
                 </div>
+                <Logo />
             </div>
     )
 }
